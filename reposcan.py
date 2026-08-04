5191dca45096f38172a40574e68abf8a861a4a32
Nafis266 <nafis.niyas@gmail.com>
2026-08-05 01:41:19 +0530

    added fn to scan repo for files

---
+++ reposcan.py
@@ -0,0 +1,34 @@
+import os
+
+S_EXTENSIONS = (
+    ".py",
+    ".java",
+    ".cpp",
+    ".c",
+    ".h",
+)
+
+def scan_repository(repo_path):
+    files = []
+
+    for root, dirs, filenames in os.walk(repo_path):
+        dirs[:] = [d for d in dirs if not d.startswith(".")]
+        for filename in filenames:
+            if filename.endswith(S_EXTENSIONS):
+                file_path = os.path.join(root, filename)
+
+                files.append({
+                    "name": filename,
+                    "path": file_path
+                })
+    return files
+
+
+repo_path = "./ex_repo"
+
+repo_files = scan_repository(repo_path)
+
+print("Files found:")
+
+for file in repo_files:
+    print(file["path"])
